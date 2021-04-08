2158491ae97af9f42410e5e36567f871a0630898
jwong39 <59982624+jwong39@users.noreply.github.com>
2021-04-08 12:28:11 -0500

    Add files via upload

---
+++ beautifulSoup.js
@@ -0,0 +1,18 @@
+// var tableElem, rowElem, colElem;
+// var r = 2;
+// var c = 2;
+// document.getElementById("myTable").innerHTML = "";
+// alert("Please enter some numeric value"); 
+// tableElem = document.getElementById('myTable');
+// for (var i = 0; i < r; i++) {
+//     rowElem = document.createElement('tr');
+
+//     for (var j = 0; j < c; j++) {
+//         colElem = document.createElement('td');
+//         //colElem.appendChild(document.createTextNode(j + 1));
+//         rowElem.appendChild(colElem);
+//     }
+//     tableElem.appendChild(rowElem);
+    
+// }
+// document.body.appendChild(tableElem);
